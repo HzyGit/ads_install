6282159d5a0b8dcaca47603e142910b751a0ef80
hzy <hzy.oop@gmail.com>
2015-06-23 20:44:42 +0800

    增加命令行参数解析代码
    支持-h,-f等命令行

--- install.py
+++ install.py
@@ -3,6 +3,7 @@ import tarfile
 import os
 import sys
 import functools
+import getopt
 
 def install(tarf,dest):
 	tar=tarfile.open(tarf,"r:gz");
@@ -22,7 +23,27 @@ def test():
 	install(sys.argv[1],sys.argv[2])
 
 def main():
-	test()
+	try:
+		opts,args=getopt.getopt(sys.argv[1:],'hf:',["help","file="])
+	except getopt.GetoptError:
+		print "getopt error"
+		usage()
+		sys.exit(1)
+	path=os.getcwd()
+	for opt,arg in opts:
+		if opt in ("-h","--help"):
+			usage()
+			sys.exit()
+		elif opt in ( "-f","--file"):
+			tarfile=arg
+	if len(tarfile) ==0:
+		print "cannot find tarfile"
+		usage()
+		sys.exit(3)
+	if len(args) == 1:
+		path=args[0]
+	install(tarfile,path)
+
 
 if __name__ == "__main__":
 	main()
